282639034bd624defa07244198f412cc2e14bebc
enfusq <aivars.trumars@gmail.com>
2025-11-12 08:39:14 +0200

    Added blink.cmp for autocompletion

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,5 @@
 vim.lsp.config('intelephense', {
+	capabilities = require('blink.cmp').get_lsp_capabilities(),
 	cmd = { 'intelephense', '--stdio' },
 	filetypes = { 'php' },
 	root_dir = vim.fs.dirname(vim.fs.find({ 'compser.json', '.git' }, { upwards = true })[1]),
@@ -12,6 +13,7 @@ vim.lsp.config('intelephense', {
 })
 
 vim.lsp.config('tsserver', {
+	capabilities = require('blink.cmp').get_lsp_capabilities(),
 	cmd = { 'typescript-language-server', '--stdio' },
 	filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
 	root_dir = vim.fs.dirname(vim.fs.find({ 'tsconfig.json', 'package.json', '.git' }, { upward = true })[1]),
